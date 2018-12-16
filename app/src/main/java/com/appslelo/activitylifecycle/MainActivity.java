902ae72dd3e2b25e68725d1613b4a258c8821002
tsfapps <appslelo.com@gmail.com>
2018-12-16 12:14:52 +0530

    Fixed the bugs

--- app/src/main/java/com/appslelo/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/appslelo/activitylifecycle/MainActivity.java
@@ -1,6 +1,8 @@
 package com.appslelo.activitylifecycle;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonA;
+    private Button buttonDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         buttonA = findViewById(R.id.btn_a);
         buttonA.setOnClickListener(this);
+        buttonDialog = findViewById(R.id.btn_dialog);
+        buttonDialog.setOnClickListener(this);
         Toast.makeText(this, "onCreate of Activity A", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Activity A Launched", Toast.LENGTH_SHORT).show();
     }
@@ -72,13 +77,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(this, ActivityB.class);
-        startActivity(intent);
+        switch (view.getId()) {
+            case R.id.btn_a:
+            Intent intent = new Intent(this, ActivityB.class);
+            startActivity(intent);
+            break;
+            case R.id.btn_dialog:
+                dialogShow();
+                break;
+        }
     }
-
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    private void dialogShow(){
+        Toast.makeText(this, "Dialog Clicked", Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Alert Dialog Shown");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
 
